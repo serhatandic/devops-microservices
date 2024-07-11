18a7fb9d811c5e3e379e19ca3b6dcdd175a9cea9
serhat andic <serhatandic42@gmail.com>
2024-07-11 17:05:42 +0300

    Add unit tests for user-service and product-service

---
+++ product-service/test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+	res.send('Hello from Product Service!');
+});
+
+describe('GET /', () => {
+	it('responds with "Hello from Product Service!"', (done) => {
+		request(app)
+			.get('/')
+			.expect('Hello from Product Service!')
+			.expect(200, done);
+	});
+});
---
+++ user-service/test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+	res.send('Hello from User Service!');
+});
+
+describe('GET /', () => {
+	it('responds with "Hello from User Service!"', (done) => {
+		request(app)
+			.get('/')
+			.expect('Hello from User Service!')
+			.expect(200, done);
+	});
+});
