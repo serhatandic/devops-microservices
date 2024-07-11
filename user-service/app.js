191a3cf0e1f425efe39c03304ff4d2bb2bfd176c
serhat andic <serhatandic42@gmail.com>
2024-07-11 16:33:27 +0300

    checking if my jenkins pipeline is going to work - 2

--- user-service/app.js
+++ user-service/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello from User Service! - updated');
+	res.send('Hello from User Service! - updated2');
 });
 
 app.listen(3000, () => {
