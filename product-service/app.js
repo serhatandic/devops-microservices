9a7b9b6bdd79dd244e40fa57f688112f949b451f
serhat andic <serhatandic42@gmail.com>
2024-07-11 11:51:53 +0300

    Add user-service, product-service, and Kubernetes manifests

---
+++ product-service/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello from Product Service!');
+});
+
+app.listen(3001, () => {
+	console.log('Product service listening on port 3001');
+});
---
+++ user-service/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello from User Service!');
+});
+
+app.listen(3000, () => {
+	console.log('User service listening on port 3000');
+});
